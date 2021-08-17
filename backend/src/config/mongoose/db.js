e91b8eff191d7052694e3a05a8614a8499b936d0
jhonatanffelipe <jhonatanfnm@gmail.com>
2021-08-16 21:44:52 -0300

    Criando o projeto ReactJs e organizando a estrutura de pastas e arquivos

--- backend/src/config/mongoose/db.js
+++ backend/src/config/mongoose/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://127.0.0.1:27017/todo'
+const url = 'mongodb://localhost:27017/todo'
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+function Home() {
+  return (
+    <h1>Olá mundo</h1>
+  );
+}
+
+export default Home;
