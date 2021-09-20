ba2039b3dc656020c02e274991e8edca1ad293ea
jhonatanffelipe <jhonatanfnm@gmail.com>
2021-09-20 15:57:38 -0300

    Criação do projeto, tela home e estrutura de pastas de arquivo

---
+++ mobile/src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native"
+
+import style from "./style";
+
+export default function Home() {
+  return (
+    <View style={style.container}>
+      <Text >Bem vindo a tela Home</Text>
+    </View>
+  )
+}
---
+++ mobile/src/pages/Home/style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+export default style
